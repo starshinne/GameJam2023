b7a37b103a08279b307f3d78a2dde144c39fe25d
starshinne <1326239494@qq.com>
2023-07-08 12:03:45 +0800

    202307081203

--- Assets/Scripts/PaperPlane.cs
+++ Assets/Scripts/PaperPlane.cs
@@ -1,12 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PaperPlane : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update 
     public Rigidbody2D rb;
-    private void Awake() {
-        rb =GetComponent<Rigidbody2D>();
+    [Header("Basic Data")]
+    public float speed;
+    public float jumpForce;
+    public InputControl inputControl;
+
+    private void Awake()
+    {
+        inputControl = new InputControl();
+        rb = GetComponent<Rigidbody2D>();
+      inputControl.Player.Jump.started += Jump;
+
     }
+    private void OnEnable() {
+        inputControl.Enable();
+    }
+    private void OnDisable() {
+        inputControl.Disable();
+    }
+    private void Update()
+    {
+        planeMove();
+      
+
+    }
+
+    private void planeMove()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {      
+            rb.AddForce(transform.up*jumpForce,ForceMode2D.Impulse);       
+    }
+
 }
